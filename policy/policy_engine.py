9b36ba06ee1f41cf75bcfdfd784c64f8ee19224c
CapLWS-SLC <legacygstudios@outlook.com>
2026-07-06 17:35:37 -0400

    Refactor PolicyEngine and add PolicyResult class

--- policy/policy_engine.py
+++ policy/policy_engine.py
@@ -1,10 +1,17 @@
-# policy/policy_engine.py
+# jarvondis/policy/policy_engine.py
+
+class PolicyResult:
+    def __init__(self, allowed: bool, message: str = ""):
+        self.allowed = allowed
+        self.blocked = not allowed
+        self.message = message
+
 
 class PolicyEngine:
-    def __init__(self, rule_sets):
-        self.rules = rule_sets
+    def __init__(self, rules):
+        self.rules = rules
 
-    def apply(self, message):
+    def apply(self, message: str) -> PolicyResult:
         for rule in self.rules:
             result = rule.evaluate(message)
             if result.blocked:
